d954ea74b92930e1d6e6a47be1b04a1e15264d04
Len-En Chiu <linda34264622@gmail.com>
2021-11-10 20:07:06 +0800

    route跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',function(){
     return'welcome';
 });
+
+Route::get('r1', function() {
+    return redirect('r2');
+});
+
+Route::get('r2', function() {
+    return view('welcome');
+});
+
